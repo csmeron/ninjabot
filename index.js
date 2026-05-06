379610d43b5298ca7c964b6ae8d0153a3362292b
csmeron <csmeron@proton.me>
2026-05-06 04:14:52 -0500

    incomplete wip

---
+++ index.js
@@ -0,0 +1,46 @@
+// Load Node's file system and path util modules
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Require the necessary Discord.js classes and the bot's token
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+
+// Create a new client instance
+// `GatewayIntentBits.Guilds` ensures caches are populated and available
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with the client token
+client.login(token);
+
+
+// Create a collection to track commands
+client.commands = new Collection();
+
+// foldersPath scans for command categories, commandFolders
+// checks for command files in said paths
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// For each command file detected, add them to the command collection
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with they key as the cmd name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    }
+    else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+}

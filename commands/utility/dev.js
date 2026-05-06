a2bf53cbf5edfd212c14a3767275814c6f975f1d
csm <csmeron@proton.me>
2026-05-06 03:45:35 -0500

    added basic commands

---
+++ commands/utility/dev.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('dev').setDescription('Who developed this bot?'),
+  async execute(interaction) {
+    await interaction.reply('@617478275320643655');
+  },
+};

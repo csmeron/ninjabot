acb4415fa73a6da17b68f8110ea73ac2dbc1dfb4
csmeron <csmeron@proton.me>
2026-05-13 18:24:52 -0500

    Revise center.js to distinguish between both centers

--- commands/utility/center.js
+++ commands/utility/center.js
@@ -1,9 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, Guild } = require('discord.js');
+let server = '';
+if (Guild.name.endsWith('press)')) {
+  server = 'Spring-Cypress';
+} else {
+  server = 'Spring-Rayford';
+}
 
 module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder().setName('center').setDescription('Which center is this server for?'),
   async execute(interaction) {
-    await interaction.reply('idk');
+    await interaction.reply(`This is the server for the ${server} center!`);
   },
 };

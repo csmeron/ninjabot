5a1e0589bf35e00654d8bdb048ef5bcf2a310acd
csmeron <csmeron@proton.me>
2026-05-14 16:49:12 -0500

    WIP

--- events/joinsync.js
+++ events/joinsync.js
@@ -2,12 +2,28 @@ const { Events } = require('discord.js');
 const client = require('../index.js');
 // console.log(client);
 module.exports = {
-  name: Events.MessageReactionAdd,
-  async execute(reaction, user) {
-    console.log('reaction event fired');
+  name: Events.GuildMemberAdd,
+  async execute(member) {
+    // verify event occurring
+    console.log('Member Join Detected');
+    // fetch appropriate log channels
     const dev1 = await client.channels.fetch('1504344481489489920');
     const dev2 = await client.channels.fetch('1504344463126822943');
-    await dev1.send('confirmed');
-    await dev2.send('confirmed');
+    // log new user's tag (user#1234), displayname (nickname), and flakeid
+    const newUserTag = member.user.tag;
+    const newUserNick = member.displayName;
+    const newUserSnowflake = member.id;
+
+    // verify which center server they joined
+    let center = '';
+    if (member.guild.name.endsWith('press)')) {
+      center = 'Spring-Cypress';
+    } else if (member.guild.name.endsWith('ev2')) {
+      center = 'Spring-Rayford';
+    }
+
+    // send confirmation message
+    await dev1.send(`${newUserNick} has joined the ${center} server. `);
+    await dev1.send(`${newUserNick} has joined the ${center} server. `);
   },
 };
